06ef3906e1801f3f6a2ea904f486e61d4390054d
Laura Marson <laura.marson@zup.com.br>
2022-09-16 17:17:26 -0300

    projetos

---
+++ bytebank-encapsulado/src/TesteSacaNegativo.java
@@ -0,0 +1,13 @@
+
+public class TesteSacaNegativo {
+	public static void main(String[] args) {
+		Conta conta = new Conta(123, 456);
+		conta.deposita(100);
+		System.out.println(conta.saca(101));
+
+		conta.saca(101);
+
+		System.out.println(conta.getSaldo());
+
+	}
+}
